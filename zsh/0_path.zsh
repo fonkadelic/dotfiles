198f331cbdee83ac821fb22b3f40ccdfdbe8052a
Bernhard Loibl <bernhard.loibl@googlemail.com>
2013-03-10 16:03:23 +0100

    added npm path

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,2 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/usr/local/sbin:/usr/local/bin:$PATH:~/.dotfiles/bin
+export PATH=/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:$PATH:~/.dotfiles/bin
