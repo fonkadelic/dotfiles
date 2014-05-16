52c1fc49728592f4283d1666aefb8f18c753f04a
Bernhard Loibl <bernhard.loibl@googlemail.com>
2014-05-16 21:50:47 +0200

    Adds lldb shortcuts

---
+++ lldb/scripts/shortcuts.py
@@ -0,0 +1,28 @@
+"""
+Source: https://gist.github.com/lukabernardi/11375647
+"""
+
+import lldb
+import commands
+
+def __lldb_init_module (debugger, dict):
+  debugger.HandleCommand('command script add -f LBRShortcut.json_data_command json_data')
+  debugger.HandleCommand('command script add -f LBRShortcut.fire_fault_command fire_fault')
+
+def fire_fault_command(debugger, command, result, dict):
+  # command regex fire_fault 's/(.+)/po [%1 willAccessValueForKey:nil]/'
+    if len(command) == 0:
+        print "You need to specify the name of a variable"
+        return
+
+    variable_arg = command
+    debugger.HandleCommand('expr [' + variable_arg +' willAccessValueForKey:nil]')
+
+
+def json_data_command(debugger, command, result, dict):
+  if len(command) == 0:
+    print "You need to specify the name of a variable"
+    return
+
+  variable_arg = command
+  debugger.HandleCommand('po [NSJSONSerialization JSONObjectWithData:' + variable_arg +' options:0 error:nil]')
