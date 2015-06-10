bcbb6ad5c8a2a690a025b052c188289081b1b9b5
Bernhard Loibl <bernhard.loibl@gmail.com>
2015-06-10 19:52:01 +0200

    Adds git shortcut.

--- zsh/noglob.zsh
+++ zsh/noglob.zsh
@@ -2,3 +2,19 @@
 # stuff like ga *foo* and correctly have
 # git add the right stuff
 alias git='noglob git'
+
+
+# Remove prezto alias
+unalias g
+
+# Show status if there is no command
+g() {
+  if [[ $# > 0 ]]; then
+	git $@
+  else
+	git status
+  fi
+}
+
+# Complete g like git
+compdef g=git
