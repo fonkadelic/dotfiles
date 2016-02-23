88ce2f3dfb6cea8494c669c23018d8b282528b88
Bernhard Loibl <bernhard.loibl@gmail.com>
2016-02-23 20:33:41 +0100

    Updates path.

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,2 +1,2 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/usr/local/share/npm/bin:~/.cabal/bin:/usr/local/sbin:/usr/local/bin:$PATH:~/.dotfiles/bin
+export PATH=/usr/local/share/npm/bin:~/.cabal/bin:/usr/local/sbin:/usr/local/bin:$PATH:~/.dotfiles/bin:$HOME/bin:~/Library/Python/2.7/bin
